1f87935fe2dca2910540e64c1f71689691b6ce91
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-04 19:59:02 +0545

    added db connection method

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require('./app')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.DB.replace('<db_password>', process.env.PW)
+
+
+mongoose.connect(URL).then((con) => {
+    console.log(con.connections);
+    console.log('Db connection successful')
+}).catch(err => console.log(err))
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
+
+
