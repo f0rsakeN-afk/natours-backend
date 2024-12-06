74e2960b000a689822b920ddce2d1a6b6915c053
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-06 23:32:47 +0545

    added path

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const tourRoutes = require('./routes/tourRoutes')
 
 app.use(express.json());
+
+
+app.use('/api/v1/tour', tourRoutes)
 
 module.exports = app
